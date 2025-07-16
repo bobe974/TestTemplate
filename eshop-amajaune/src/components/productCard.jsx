fd56298b203ec645fc07b4204696b62541152bfd
BAILLIF Etienne <etienne.baillif@humansconnexion.com>
2025-07-16 09:52:48 +0200

    fix classname error

--- eshop-amajaune/src/components/productCard.jsx
+++ eshop-amajaune/src/components/productCard.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const ProductCard = ({product}) => {
   return (
     <div>
-        <div classNameName="group relative">
+        <div className="group relative">
         <img src={product.image} className="aspect-square  rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80" />
         <div className="mt- flex justify-between">
           <div>
             <h3 className="text-sm text-gray-700">
               <a href="#">
-                <span aria-hidden="true" class="absolute inset-0"></span>
+                <span aria-hidden="true" className="absolute inset-0"></span>
                 {product.nom}
               </a>
             </h3>
